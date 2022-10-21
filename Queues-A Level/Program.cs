6c864ae0849ca4a3e8fec1f1a64096f55a8f3104
Adam Krawczyk <Adam882200@gmail.com>
2022-10-21 10:34:22 +0100

    Add project files.

---
+++ Queues-A Level/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues_A_Level
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Queue<string> MyQ = new Queue<string>();
+			MyQ.Enqueue("Abc");
+			MyQ.Enqueue("Def");
+			MyQ.Enqueue("Ghj");
+
+			Console.WriteLine("Queue Demonstration");
+			Console.WriteLine($"Queue Count: {MyQ.Count}");
+			Console.WriteLine($"Dequeued: {MyQ.Dequeue()}");
+			Console.WriteLine($"Queue Count after dequeue: {MyQ.Count}");
+
+			Console.WriteLine("For each through the rest");
+			foreach (string StringTing in MyQ)
+			{
+				Console.WriteLine($"More String Queues: {StringTing}");
+			}
+
+		}
+	}
+}
